fb10deb10586010f72c89f77fb4d27cdac01047d
Daniel Chamorro <dan@dcitllc.com>
2018-11-16 22:00:19 -0500

    Tested OAuth and got sucessfull connection to google, after sign in redirect to our call back url

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 // Import Express
 const express = require("express");
+// Require Passport
+const passport = require("passport");
+// Google Strategy
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+// Keys file
+const keys = require("./config/keys");
+
 // Create application
 const app = express();
 
-// Route Handler
-app.get("/", (req, res) => {
-  res.send({ hi: "buddy" });
-});
+// Create new instance of Google Strategy
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
+
+// Pass this route to Passport to handle the flow
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"] // The scope tells google what we want access to
+  })
+);
 
 // Add port ENV
 const PORT = process.env.PORT || 5000;
