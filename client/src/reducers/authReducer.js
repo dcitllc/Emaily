ef8d54677d89661da96d52ba332697dda8c14691
Daniel Chamorro <dan@dcitllc.com>
2018-11-20 15:19:40 -0500

    Added Redux, and actions. Refactor app to be a class component to make use of life cycle methods

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,5 +1,6 @@
 // Create reducer with default state being an empty object
 export default function(state = {}, action) {
+  console.log(action);
   switch (action.type) {
     default:
       return state;
