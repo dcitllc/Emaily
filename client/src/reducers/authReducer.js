94ff80ceb26b19b1ea0eec5913a5957cb2753100
Daniel Chamorro <dan@dcitllc.com>
2018-11-23 15:56:26 -0500

    Header Content

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,7 +1,10 @@
+import { FETCH_USER } from "../actions/types";
+
 // Create reducer with default state being an empty object
-export default function(state = {}, action) {
-  console.log(action);
+export default function(state = null, action) {
   switch (action.type) {
+    case FETCH_USER:
+      return action.payload || false;
     default:
       return state;
   }
