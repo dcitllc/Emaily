7cc90879fe327b5490dfd331c0b4c8e232d96539
Daniel Chamorro <dan@dcitllc.com>
2018-11-23 16:04:39 -0500

    Redirect User on Auth

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -10,8 +10,7 @@ class Header extends Component {
       case false:
         return (
           <li>
-            <a href="/auth/google" />
-            Login with Google
+            <a href="/auth/google">Login with Google</a>
           </li>
         );
 
